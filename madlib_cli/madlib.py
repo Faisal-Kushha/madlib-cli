7aa609fed25b5956b3d8cc5c5e3275b40806f42c
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-12 21:19:53 +0300

    Done

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,45 @@
+import re
+import errno
+import os
+
+
+def read_template(game_file):
+    try:
+        with open(game_file) as file:
+            file_content = file.read()
+            return file_content
+    except:
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), game_file)
+
+
+def parse_template(text):
+    parse_text = re.findall(r'\{(.*?)\}', text)
+    for i in parse_text:
+        text = text.replace((i), "", 1)
+    return text, tuple(parse_text)
+
+
+def merge(text, parse_text):
+    new_text = text.format(*parse_text)
+    with open('assets/test.txt', 'w') as output:
+        output.write(new_text)
+    return new_text
+
+
+def game():
+    print("Welcome to the Madlib game")
+    print(" In the Madlib game you have to enter some answers that will be used in the game, and it will return to you a phrase that contain all your answers in a funny way.")
+
+    x = read_template('../assets/main_text.txt')
+    text, parse_text = parse_template(x)
+    input = []
+    for i in parse_text:
+        user_input = input(f'Please enter a {i}')
+        input.append(user_input)
+    result = merge(text, input)
+    return result
+
+
+if __name__ == '__main__':
+    print(game())
