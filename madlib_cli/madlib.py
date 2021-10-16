e41cc2b8bb900a5f42705cdda066933dd9193524
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-16 16:38:52 +0300

    Add Doc String

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -4,6 +4,9 @@ import os
 
 
 def read_template(game_file):
+    """
+    A function that will open the file using the file path and read it. And return the text inside the file. if the file path was wrong it will return an error.
+    """
     try:
         with open(game_file) as file:
             file_content = file.read()
@@ -14,6 +17,9 @@ def read_template(game_file):
 
 
 def parse_template(text):
+    """
+    A function that will take the text from the previous one and analyze it. It will put anything inside the curly brackets in a tuple and remove it from the text.
+    """
     parse_text = re.findall(r'\{(.*?)\}', text)
     for i in parse_text:
         text = text.replace((i), "", 1)
@@ -21,6 +27,9 @@ def parse_template(text):
 
 
 def merge(text, parse_text):
+    """
+    A function that will merge the user inputs with the text by using the format method.
+    """
     new_text = text.format(*parse_text)
     with open('../assets/test.txt', 'w') as output:
         output.write(new_text)
@@ -28,6 +37,9 @@ def merge(text, parse_text):
 
 
 def game():
+    """
+    A function that will run the game, print a welcoming message and return the formatted text based on the user inputs.
+    """
     print("###########################################")
     print("Welcome to the Madlib game")
     print(" In the Madlib game you have to enter some answers that will be used in the game, and it will return to you a phrase that contain all your answers in a funny way.")
